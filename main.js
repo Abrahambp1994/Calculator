5664eac33ae9e7fd79e0a8b7ff9bf811f4615f9e
Abraham Botica Pantoja <Abrahambp1994@icloud.com>
2023-06-14 18:34:08 +0200

    Trabajo terminado
    Comentarios añadidos

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
+//Selecciona pantalla de la calculadora
 let display = document.getElementById("display");
 
+//Agrega el carácter al final del display
 function appendCharacter(character) {
   display.value += character;
 }
-
+//Borra el contenido del display
 function clearDisplay() {
   display.value = "";
 }
-
+//Borra el último carácter
 function deleteLastCharacter() {
   display.value = display.value.slice(0, -1);
 }
 
-function deleteAllCharacters() {
-  display.value = "";
-}
-
+//Calcula el resultado
 function calculateResult() {
   let result;
   try {
